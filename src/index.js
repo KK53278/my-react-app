f1427738d62000b2944d18ee505ad5f6d3ca1e70
EC2 Default User <110440486+KK53278@users.noreply.github.com>
2022-12-27 07:04:20 +0000

    first init

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import useFetch from "./useFetch";
+import ReactDOM from "react-dom/client";
+
+const Home = () => {
+  const [data] = useFetch("https://jsonplaceholder.typicode.com/todos");
+
+  return (
+    <>
+      {data &&
+        data.map((item) => {
+          return <p key={item.id}>{item.title}</p>;
+        })}
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Home />);
